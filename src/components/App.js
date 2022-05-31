8134b5feebe126345749c52971a7f1a5669d4d84
Christopher Callender <christophercallender0000@gmail.com>
2022-05-30 20:42:04 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from 'react';
+import ShoppingList from './ShoppingList';
+import itemData from '../data/items';
 
 function App() {
+   const [darkSet, setDark] = useState(true);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+   const handleTheme = () => setDark(!darkSet);
 
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+   return (
+      <div className={'App ' + (darkSet ? 'light' : 'dark')}>
+         <header>
+            <h2>Shopster</h2>
+            <button onClick={handleTheme}>
+               {darkSet ? 'Dark' : 'Light'} Mode
+            </button>
+         </header>
+         <ShoppingList items={itemData} />
+      </div>
+   );
 }
 
 export default App;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,32 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react';
+import Item from './Item';
 
 function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+   const [categorySet, setCategory] = useState('All');
+
+   const allItems = items.filter((item) => {
+      return categorySet === 'All' ? item : item.category === categorySet;
+   });
+
+   const handleChange = (e) => setCategory(e.target.value);
+
+   const selectedItems = allItems.map((item) => (
+      <Item key={item.id} name={item.name} category={item.category} />
+   ));
+
+   return (
+      <div className="ShoppingList">
+         <div className="Filter">
+            <select name="filter" onChange={handleChange}>
+               <option value="All">Filter by category</option>
+               <option value="Produce">Produce</option>
+               <option value="Dairy">Dairy</option>
+               <option value="Dessert">Dessert</option>
+            </select>
+         </div>
+         <ul className="Items">{selectedItems}</ul>
       </div>
-      <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+   );
 }
 
 export default ShoppingList;
